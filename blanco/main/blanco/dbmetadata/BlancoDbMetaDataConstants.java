de9afeb5eea5fbb099d28480ae27e87f8ff61f3f
tueda <ueo@ueo.co.jp>
2020-10-07 19:47:53 +0900

    [POI4] 2.2.0: Now ready to use apache poi 4.1.2.

--- blanco/main/blanco/dbmetadata/BlancoDbMetaDataConstants.java
+++ blanco/main/blanco/dbmetadata/BlancoDbMetaDataConstants.java
@@ -20,7 +20,7 @@ public class BlancoDbMetaDataConstants {
      * 項目番号:3<br>
      * バージョン番号。
      */
-    public static final String VERSION = "0.1.5";
+    public static final String VERSION = "2.2.0";
 
     /**
      * 項目番号:4<br>
